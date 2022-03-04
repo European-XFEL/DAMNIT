66267b964e37fb096d00a037abc065f827048e9e
Thomas Kluyver <thomas@kluyver.me.uk>
2022-03-04 14:59:33 +0000

    Code to test processing migration notifications

---
+++ amore_mid_prototype/backend/test_listener.py
@@ -0,0 +1,40 @@
+import logging
+from dataclasses import dataclass
+from datetime import datetime
+from glob import glob
+
+from .listener import EventProcessor
+
+log = logging.getLogger(__name__)
+
+@dataclass
+class DummyRecord:
+    timestamp: int
+
+class TestEventProcessor(EventProcessor):
+    def run(self):
+        while True:
+            try:
+                line = input('proposal run: ')
+                prop, run = [int(x) for x in line.split()]
+                path = glob(f'/gpfs/exfel/exp/*/*/p{prop:06}/raw/r{run:04}')[0]
+                inst, cycle = path.split('/')[4:6]
+                msg = {'proposal': prop, 'run': run, 'path': path,
+                       'instrument': inst, 'cycle': cycle}
+                record = DummyRecord(timestamp=int(datetime.utcnow().timestamp() * 1000))
+                self.handle_migration_complete(record, msg)
+            except Exception:
+                log.error("Error processing event", exc_info=True)
+
+
+def listen_migrated():
+    try:
+        with EventProcessor() as processor:
+            processor.run()
+    except KeyboardInterrupt:
+        print("Stopping on Ctrl-C")
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    listen_migrated()
