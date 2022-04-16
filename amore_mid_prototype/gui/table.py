70cac58099b949188ffc4510e5476d30577945b9
JamesWrigley <james@puiterwijk.org>
2022-04-16 19:22:08 +0200

    Prettify displaying numbers

--- amore_mid_prototype/gui/table.py
+++ amore_mid_prototype/gui/table.py
@@ -151,7 +151,15 @@ class Table(QtCore.QAbstractTableModel):
                 return time.strftime("%H:%M:%S %d/%m/%Y", time.localtime(value))
 
             elif pd.api.types.is_float(value):
-                return str("{:.6f}".format(value))
+                if value % 1 == 0:
+                    # If it has no decimal places, display it as an int
+                    return f"{int(value)}"
+                elif 0.1 < value < 10_000:
+                    # If it's an easily-recognized range for numbers, display as a float
+                    return f"{value:.3f}"
+                else:
+                    # Otherwise, display in scientific notation
+                    return f"{value:.3e}"
 
             else:
                 return str(value)
