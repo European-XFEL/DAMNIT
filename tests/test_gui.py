9e5750e7f1007caa8ad9ff76326f6ec720a21bc0
JamesWrigley <james@puiterwijk.org>
2022-11-01 21:42:28 +0100

    Add some tests for MainWindow.handle_update()

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,6 +1,7 @@
 from unittest.mock import patch
 
 from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtCore import Qt
 
 from amore_mid_prototype.gui.main_window import MainWindow
 
@@ -81,3 +82,42 @@ def test_editor(mock_db, mock_ctx, qtbot):
         win.close()
         assert win.isHidden()
         assert ctx_path.read_text() == new_code
+
+def test_handle_update(mock_db, qtbot):
+    db_dir, db = mock_db
+
+    win = MainWindow(db_dir, False)
+
+    # Helper lambdas
+    model = lambda: win.table_view.model()
+    get_headers = lambda: [win.table_view.model().headerData(i, Qt.Horizontal)
+                           for i in range(win.table_view.horizontalHeader().count())]
+
+    # Sending an update should add a row to the table
+    msg = {
+        "Proposal": 1234,
+        "Run": 1,
+        "scalar1": 42,
+        "string": "foo"
+    }
+
+    assert win.data.shape[0] == 0
+    win.handle_update(msg)
+    assert win.data.shape[0] == 1
+    assert win.table_view.model().rowCount() == 1
+
+    # Columns should be added for the new variables
+    headers = get_headers()
+    assert "Scalar1" in headers
+    assert "string" in headers
+
+    # Send an update for an existing row
+    msg["scalar1"] = 43
+    win.handle_update(msg)
+    assert model().data(model().index(0, headers.index("Scalar1"))) == str(msg["scalar1"])
+
+    # Add a new column to an existing row
+    msg["array"] = 3.14
+    win.handle_update(msg)
+    assert len(headers) + 1 == len(get_headers())
+    assert "Array" in get_headers()
