3da8f383e55d27803c648984c46ba7f41af2953b
Thomas Kluyver <thomas@kluyver.me.uk>
2022-12-08 14:35:20 +0000

    Use open() function instead of Path.open()

--- amore_mid_prototype/backend/supervisord.py
+++ amore_mid_prototype/backend/supervisord.py
@@ -63,7 +63,7 @@ def write_supervisord_conf(root_path):
 
     # Create supervisord.conf
     config = configparser.ConfigParser()
-    with (Path(__file__).parent / "supervisord.conf").open('r') as f:
+    with open(Path(__file__).parent / "supervisord.conf", 'r') as f:
         config.read_file(f)
     config["inet_http_server"]["port"] = str(port)
     config["inet_http_server"]["username"] = username
