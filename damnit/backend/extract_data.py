c082f3e50718d9fba2e2643c2fce9223859c78f1
JamesWrigley <james@puiterwijk.org>
2023-02-11 22:06:07 +0100

    Make the slurm timeout configurable
    And increase the default timeout to two hours.

--- damnit/backend/extract_data.py
+++ damnit/backend/extract_data.py
@@ -152,10 +152,15 @@ class Extractor:
         return self._proposal
 
     def slurm_options(self):
+        opts = ["--time", self.db.metameta.get("slurm_time", "02:00:00")]
+
         if reservation := self.db.metameta.get('slurm_reservation', ''):
-            return ['--reservation', reservation]
-        partition = self.db.metameta.get('slurm_partition', '') or default_slurm_partition()
-        return ['--partition', partition]
+            opts.extend(['--reservation', reservation])
+        else:
+            partition = self.db.metameta.get('slurm_partition', '') or default_slurm_partition()
+            opts.extend(['--partition', partition])
+
+        return opts
 
     def extract_and_ingest(self, proposal, run, cluster=False,
                            run_data=RunData.ALL, match=()):
