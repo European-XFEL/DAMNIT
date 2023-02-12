fe6785ce723278c3a7f14e22bb92ffcf2a02585c
JamesWrigley <james@puiterwijk.org>
2023-02-12 21:05:46 +0100

    Add a command to migrate databases with old image summaries
    Now that the GUI expects full RGBA thumbnails as image summaries, we have to
    provide a way for existing databases to upgrade. I considered supporting both
    kinds of summaries in the GUI, but decided that in the long run it'd be simpler
    if we just upgraded databases instead of going for full backwards-compatibility
    in the GUI.

---
+++ tests/test_migrations.py
@@ -0,0 +1,45 @@
+import pickle
+
+import h5py
+import numpy as np
+
+from amore_mid_prototype.migrations import migrate_images
+from amore_mid_prototype.backend.extract_data import add_to_db
+
+
+def test_migrate_images(mock_db, monkeypatch):
+    db_dir, db = mock_db
+    monkeypatch.chdir(db_dir)
+
+    proposal = 1234
+    db.metameta["proposal"] = proposal
+
+    extracted_data_dir = db_dir / "extracted_data"
+    extracted_data_dir.mkdir()
+
+    # Create fake HDF5 file and SQLite rows with old 2D image summaries
+    run = 42
+    mock_h5_path = extracted_data_dir / f"p{proposal}_r{run}.h5"
+    foo = np.random.rand(100, 100)
+    bar = 42
+    with h5py.File(mock_h5_path, "w") as f:
+        f.create_dataset(".reduced/foo", data=foo)
+        f.create_dataset(".reduced/bar", data=np.array(bar))
+    add_to_db({"foo": foo, "bar": bar}, db.conn, proposal, run)
+
+    migrate_images(db, db_dir)
+
+    # Now the summary should be a 3D RGBA image, and the non-image summary
+    # should be unchanged.
+    with h5py.File(mock_h5_path) as f:
+        assert f[".reduced/foo"].shape == (*foo.shape, 4)
+        assert f[".reduced/foo"].dtype == np.uint8
+        assert f[".reduced/bar"][()] == bar
+
+    cursor = db.conn.execute("SELECT * FROM runs")
+    row = cursor.fetchone()
+    thumbnail = pickle.loads(row["foo"])
+    assert isinstance(thumbnail, np.ndarray)
+    assert thumbnail.shape == (*foo.shape, 4)
+    assert thumbnail.dtype == np.uint8
+    assert row["bar"] == bar
