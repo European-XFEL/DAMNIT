bfc4ba63fa0bd357457e101056c18815bfc0e618
Thomas Kluyver <thomas@kluyver.me.uk>
2023-06-08 12:31:54 +0100

    Find proposal dirs in a thread

--- damnit/gui/open_dialog.py
+++ damnit/gui/open_dialog.py
@@ -1,33 +1,58 @@
 import os.path
 
 from extra_data.read_machinery import find_proposal
-from PyQt5.QtGui import QIntValidator
+from PyQt5.QtCore import QObject, QThread, pyqtSignal
 from PyQt5.QtWidgets import QDialog, QFileDialog, QDialogButtonBox
 
 from .open_dialog_ui import Ui_Dialog
 
+class ProposalFinder(QObject):
+    find_result = pyqtSignal(str, str)
+
+    def find_proposal(self, propnum: str):
+        if str.isdecimal() and len(str) >= 4
+            try:
+                dir = find_proposal(f"p{int(propnum):06}")
+            except:
+                dir = ''
+        else:
+            dir = ''
+        self.find_result.emit(propnum, dir)
+
 class OpenDBDialog(QDialog):
+    proposal_num_changed = pyqtSignal(str)
+    proposal_dir = ''
+
     def __init__(self):
         super().__init__()
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
         self.ui.buttonBox.button(QDialogButtonBox.StandardButton.Ok).setEnabled(False)
         self.ui.proposal_rb.toggled.connect(self.update_ok)
-        self.ui.proposal_edit.textChanged(self.update_ok)
         self.ui.folder_edit.textChanged(self.update_ok)
         self.ui.browse_button.clicked.connect(self.browse_for_folder)
-        self.ui.proposal_edit.setValidator(QIntValidator(1000, 999999))
         self.ui.proposal_edit.setFocus()
 
-    def get_proposal_dir(self):
-        if not self.ui.proposal_edit.hasAcceptableInput():
-            return None
-        prop_no = int(self.ui.proposal_edit.text())
-        return find_proposal(f"p{prop_no:06}")
+        self.proposal_finder_thread = QThread()
+        self.proposal_finder = ProposalFinder()
+        self.proposal_finder.moveToThread(self.proposal_finder_thread)
+        self.ui.proposal_edit.textChanged(self.proposal_finder.find_proposal)
+        self.proposal_finder.find_result.connect(self.proposal_dir_result)
+        self.finished.connect(self.proposal_finder_thread.quit)
+
+    def proposal_dir_result(self, propnum, dir):
+        if propnum != self.ui.proposal_edit.text():
+            return  # Text field has been changed
+        self.proposal_dir = dir
+        if self.ui.proposal_rb.isChecked():
+            self.ui.buttonBox.button(QDialogButtonBox.StandardButton.Ok).setEnabled(bool(dir))
+
 
     def update_ok(self):
-        dir = self.get_chosen_dir()
-        valid = (dir is not None) and os.path.isdir(dir)
+        if self.ui.proposal_rb.isChecked():
+            valid = bool(self.proposal_dir)
+        else:
+            valid = os.path.isdir(self.ui.folder_edit.text())
         self.ui.buttonBox.button(QDialogButtonBox.StandardButton.Ok).setEnabled(valid)
 
     def browse_for_folder(self):
@@ -37,7 +62,7 @@ class OpenDBDialog(QDialog):
 
     def get_chosen_dir(self):
         if self.ui.proposal_rb.isChecked():
-            return self.get_proposal_dir()
+            return os.path.join(self.proposal_dir, "usr/Shared/amore")
         else:
             return self.ui.folder_edit.text()
 
