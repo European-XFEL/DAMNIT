2417126e0b4a6b98046ea867fce65f6f9c19d24b
Thomas Kluyver <thomas@kluyver.me.uk>
2023-12-11 16:53:48 +0000

    Fix up some tests for recent changes

--- tests/test_migrations.py
+++ tests/test_migrations.py
@@ -5,6 +5,7 @@ import numpy as np
 
 from damnit.migrations import migrate_images
 from damnit.backend.extract_data import add_to_db
+from damnit.backend.db import BlobTypes
 
 from .helpers import reduced_data_from_dict
 
@@ -30,19 +31,19 @@ def test_migrate_images(mock_db, monkeypatch):
     add_to_db(reduced_data_from_dict({"foo": foo, "bar": bar}),
               db, proposal, run)
 
-    migrate_images(db, db_dir)
+    migrate_images(db, db_dir, dry_run=False)
 
-    # Now the summary should be a 3D RGBA image, and the non-image summary
+    # Now the summary should be a PNG image, and the non-image summary
     # should be unchanged.
     with h5py.File(mock_h5_path) as f:
-        assert f[".reduced/foo"].shape == (*foo.shape, 4)
+        assert f[".reduced/foo"].ndim == 1
         assert f[".reduced/foo"].dtype == np.uint8
+        assert f[".reduced/foo"].attrs['damnit_png'] == 1
         assert f[".reduced/bar"][()] == bar
 
     cursor = db.conn.execute("SELECT * FROM runs")
     row = cursor.fetchone()
-    thumbnail = pickle.loads(row["foo"])
-    assert isinstance(thumbnail, np.ndarray)
-    assert thumbnail.shape == (*foo.shape, 4)
-    assert thumbnail.dtype == np.uint8
+    thumbnail = row["foo"]
+    assert isinstance(thumbnail, bytes)
+    assert BlobTypes.identify(thumbnail) is BlobTypes.png
     assert row["bar"] == bar
