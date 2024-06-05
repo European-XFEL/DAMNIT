a1193ffc36e97822cf1b4a3480bbf618cb298679
Thomas Kluyver <thomas.kluyver@xfel.eu>
2024-06-05 11:49:39 +0100

    Allow non-cluster jobs 4 cores and 25 GB memory

--- damnit/backend/listener.py
+++ damnit/backend/listener.py
@@ -115,6 +115,8 @@ class EventProcessor:
             'sbatch', '--parsable',
             '--cluster=solaris',
             '-o', log_path,
+            '--cpus-per-task', '4',
+            '--mem', '25G',
             '--open-mode=append',
             # Note: we put the run number first so that it's visible in
             # squeue's default 11-character column for the JobName.
