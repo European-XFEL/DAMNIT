2418ffdb58831c84dcf114cce2c94fdd807ab036
Thomas Kluyver <thomas.kluyver@xfel.eu>
2024-08-30 15:09:40 +0100

    Add username to processing status info

--- damnit/backend/extract_data.py
+++ damnit/backend/extract_data.py
@@ -14,6 +14,7 @@ import re
 import socket
 import subprocess
 import sys
+from getpass import getuser
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from uuid import uuid4
@@ -187,6 +188,7 @@ class RunExtractor(Extractor):
             'run': run,
             'data': run_data.value,
             'hostname': socket.gethostname(),
+            'username': getuser(),
             'slurm_cluster': self._slurm_cluster(),
             'slurm_job_id': os.environ.get('SLURM_JOB_ID', ''),
         })
