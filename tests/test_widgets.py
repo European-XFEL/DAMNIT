c933778d50b28966e0232cfba0d2469908896d6a
tmichela <thomas.michelat@gmail.com>
2025-02-18 15:21:44 +0100

    test: unit tests for the ValueRangeWidget

--- tests/test_widgets.py
+++ tests/test_widgets.py
@@ -1,6 +1,6 @@
 
 import numpy as np
-from damnit.gui.widgets import PlotLineWidget
+from damnit.gui.widgets import PlotLineWidget, ValueRangeWidget
 
 
 def test_plot_line_widget(qtbot):
@@ -29,3 +29,54 @@ def test_plot_line_widget(qtbot):
     # Test edge cases
     widget.set_slider_position((x_data[0], x_data[-1]))
     widget.repaint()
+
+
+def test_value_range_widget(qtbot):
+    # Test normal case with range
+    values = np.array([1.0, 1.5, 2.0, 2.5, 3.0])
+    vmin, vmax = 1.0, 3.0
+    widget = ValueRangeWidget(values, vmin, vmax)
+    qtbot.addWidget(widget)
+
+    # Test initialization
+    assert widget.min == vmin
+    assert widget.max == vmax
+    assert widget.plot is not None
+    assert widget.slider is not None
+
+    # Test setting values
+    new_min, new_max = 1.5, 2.5
+    with qtbot.waitSignal(widget.rangeChanged) as blocker:
+        widget.set_values(new_min, new_max)
+    assert len(blocker.args) == 2
+    assert blocker.args[0] == new_min
+    assert blocker.args[1] == new_max
+    assert widget.min == new_min
+    assert widget.max == new_max
+
+    # Test input field changes
+    widget.min_input.setText("2.0")
+    with qtbot.waitSignal(widget.rangeChanged) as blocker:
+        widget.min_input.editingFinished.emit()
+    assert len(blocker.args) == 2
+    assert blocker.args[0] == 2.0
+    assert blocker.args[1] == 2.5
+    assert widget.min == 2.0
+    assert widget.plot.slider_position == (2.0, 2.5)
+
+    # Test slider changes
+    with qtbot.waitSignal(widget.rangeChanged) as blocker:
+        widget.slider.setValue((1.8, 2.8))
+    assert len(blocker.args) == 2
+    assert blocker.args[0] == 1.8
+    assert blocker.args[1] == 2.8
+    assert widget.min_input.text() == "1.8"
+    assert widget.max_input.text() == "2.8"
+
+    # Test edge case: single value
+    single_widget = ValueRangeWidget(np.array([1.0]), 1.0, 1.0)
+    qtbot.addWidget(single_widget)
+    assert single_widget.plot is None
+    assert single_widget.slider is None
+    assert single_widget.min == 1.0
+    assert single_widget.max == 1.0
