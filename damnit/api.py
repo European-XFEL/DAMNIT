26e158cc381637d5f66ffb3468c8576945e40c37
Thomas Kluyver <thomas.kluyver@xfel.eu>
2026-03-23 14:27:12 +0000

    Turn submit() back to being a plain function instead of a method

--- damnit/api.py
+++ damnit/api.py
@@ -537,31 +537,36 @@ class Damnit:
     def __repr__(self):
         return f"<Damnit database for p{self.proposal}>"
 
-    def submit(self, run: int, variables, *, provenance,
-            errors: dict[str, Exception] = None, proposal: int | None = None,
-    ):
-        """Add some results into DAMNIT's store
 
-        Args:
-            run (int): Run number
-            variables (dict): Mapping of names to arrays or DAMNIT Cell objects.
-            provenance (str): A name for what produced these results.
-            errors (dict): Mapping of names to exceptions, to make error messages
-                visible in the table.
-            proposal (int, optional): Proposal number, if not the default for
-                this DAMNIT folder.
-        """
-        from .backend.extract_data import notify_new_file
-        from .context import Cell, save_fragment
+def submit(proposal: int, run: int, variables, *, provenance,
+        errors: dict[str, Exception] = None, damnit_dir=None,
+):
+    """Add some results into DAMNIT's store
 
-        variables = {k: (v if isinstance(v, Cell) else Cell(v))
-                     for (k, v) in variables.items()}
-        errors = errors or {}
+    Args:
+        proposal (int): Proposal number
+        run (int): Run number
+        variables (dict): Mapping of names to arrays or DAMNIT Cell objects.
+        provenance (str): A name for what produced these results.
+        errors (dict): Mapping of names to exceptions, to make error messages
+            visible in the table.
+        damnit_dir (Path or str, optional): The DAMNIT directory to write into.
+            If not specified, it will find the default directory for the
+            relevant proposal.
+    """
+    from .backend.extract_data import notify_new_file
+    from .context import Cell, save_fragment
 
-        if proposal is None:
-            proposal = self._db.metameta['proposal']
+    variables = {k: (v if isinstance(v, Cell) else Cell(v))
+                 for (k, v) in variables.items()}
+    errors = errors or {}
 
-        path = save_fragment(
-            self._db_dir, proposal, run, variables, errors, provenance=provenance
-        )
-        notify_new_file(self._db_dir, proposal, run, str(path))
+    if damnit_dir is None:
+        damnit_dir = find_proposal(proposal) / "usr/Shared/amore"
+    else:
+        damnit_dir = Path(damnit_dir)
+
+    path = save_fragment(
+        damnit_dir, proposal, run, variables, errors, provenance=provenance
+    )
+    notify_new_file(damnit_dir, proposal, run, str(path))
